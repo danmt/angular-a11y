0e2a1e158982434d3a33c1c2eecb06b664fbb323
Daniel Marin <danielarturomt@gmail.com>
2019-12-21 10:43:33 -0300

    improving it a lil bit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit() {}
+export class HomeComponent {
+  articles = [
+    { title: 'Article A', type: 'type-a' },
+    { title: 'Article B', type: 'type-b' },
+    { title: 'Article C', type: 'type-c' },
+    { title: 'Article D', type: 'type-d' }
+  ];
 }
